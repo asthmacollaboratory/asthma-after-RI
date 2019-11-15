d9df6f69299e98100234115e13ffcf0df1afd8a1
Jennifer Elhawary <jennifer.liberto@ucsf.edu>
2019-11-15 14:24:36 -0800

    Create env.sh

---
+++ analysis/env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## this is very much in progress and will be completed by Thanksgiving 2019
+
+# Store any sensitive file paths, binaries, etc as variables here
+# you will source this in the run_analysis.sh
+
+# For example
+
+# binaries
+my_executable="/usr/bin/bash"
+
+# directories 
+my_dir=$(pwd)
+
+# files
+my_file="${my_dir}/.env"
+
+# any script variables
+my_password="1fish2fishRedfishBluefish"
